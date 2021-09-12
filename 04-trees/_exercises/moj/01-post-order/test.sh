aca1a152d5d238ca1e3f5c3b325c4a675e5381e3
Yudi Yamane <yudiyamane@gmail.com>
2021-09-12 00:41:57 -0300

    improved test script

--- 04-trees/_exercises/moj/01-post-order/test.sh
+++ 04-trees/_exercises/moj/01-post-order/test.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+<<comment
+Você pode executar esse script com:
+
+watch -n 0.5 --color -t  ./test.sh 
+
+Esse comando executa o script test.sh
+a cada 0.5 segundo, colorido e sem o 
+título
+
+Pra deixar esse script ainda mais top:
+- https://superuser.com/questions/181517/how-to-execute-a-command-whenever-a-file-changes
+- https://github.com/inotify-tools/inotify-tools/wikihttps://github.com/inotify-tools/inotify-tools/wiki
+comment
+
+
 # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
+
+BOLD_FMT=$(tput bold)
+NORMAL_FMT=$(tput sgr0)
 
 input_files=($(find samples/*.in -type f))
 expected_output_files=($(find samples/*.out.exp -type f))
@@ -11,19 +29,42 @@ expected_output_files=($(find samples/*.out.exp -type f))
 total_input_files=${#input_files[@]}
 
 diff_results=()
-
-# gcc placas.c -O2 -static -lm
+correct_count=0
+fail_count=0
+declare -A failed_tests
 
 for i in "${!input_files[@]}"; do
   number=$(echo ${input_files[i]} | cut -d '.' -f 1 | cut -d '/' -f 2)
   ./a.out < ${input_files[i]} > samples/$number.out
-  diff_result=$(diff -w ${expected_output_files[i]} samples/$number.out)
+  diff_result=$(diff -w --suppress-common-lines --color=always samples/$number.out ${expected_output_files[i]})
   
   if [ -z "$diff_result" ]; then
-    echo -e "$number.out ... ${GREEN}OK${NC}"
+    ((correct_count++))
   else
-    diff_results+=("$diff_result")
-    echo -e "$number.out ... ${RED}FAIL${NC}"
+    failed_tests["${input_files[i]}"]=$(echo -e "$diff_result" | tail -n +2)
+    ((fail_count++))
   fi
-  sleep 0.2
 done
+
+for failed_key in "${!failed_tests[@]}"; do 
+  echo -e "===================================="
+  echo -e "${BOLD_FMT}$failed_key${NORMAL_FMT}"
+  echo -e "${failed_tests[$failed_key]}\n"
+done
+
+RESULT_COLOR=""
+
+if [[ "$fail_count" -eq 0 ]] ; then
+  RESULT_COLOR=${GREEN}
+else
+  RESULT_COLOR=${RED}
+fi
+
+echo -e "${RESULT_COLOR}===================================="
+if [[ "$fail_count" -eq 0 ]] ; then
+  echo -e "SUCCESS:"
+else
+  echo -e "FAIL:"
+fi
+echo -e "tests passed: $correct_count/$total_input_files"
+echo -e "====================================${NC}"
